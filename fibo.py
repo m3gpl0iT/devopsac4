4e5df66836e2d8c8d0468d9cc3569a332f90ee28
m3gpl0iT <filipe.almeida@aluno.faculdadeimpacta.com.br>
2020-10-27 10:53:54 -0400

    ac4

--- fibo.py
+++ fibo.py
@@ -22,5 +22,5 @@ def ac4():
    return resposta
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6000))
+    port = int(os.environ.get("PORT", 5777))
     app.run(host='0.0.0.0', port=port) 
