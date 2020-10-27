b8480fa5fb5cdf5e6aae495e74d2aed3b612bdea
m3gpl0it <filipe.almeida@aluno.faculdadeimpacta.com.br>
2020-10-27 07:50:56 -0400

    ac4

---
+++ fibo.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+@app.route('/')
+def ac4():
+   p = 1
+   a = 0
+   limite = 50
+   contador = 0
+   resposta = "0,"
+   while (contador < limite):
+       tmp = p
+       p = p + a
+       a = tmp
+       contador=contador+1
+       resposta+= str(p) + ","
+
+
+   return resposta
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 6000))
+    app.run(host='0.0.0.0', port=port) 
